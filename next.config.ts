c62cc26714a5119a88cecc570f9e4e97bf70f0d9
gan1231 <uudam1231@gmail.com>
2026-08-11 19:05:18 +0800

    build: update next.config.ts configuration settings

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   // Docker дүрсийг жижиг байлгах — docker-compose доторх app сервис үүнийг ашиглана.
   output: "standalone",
+  serverActions: {
+    allowedOrigins: ["burtgel.dornogovi.gov.mn"],
+  },
 };
 
 export default nextConfig;
