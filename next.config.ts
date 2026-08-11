0eb8683c36f3c224cf238ac44183390de25296bf
gan1231 <uudam1231@gmail.com>
2026-08-11 19:26:35 +0800

    fix: remove merge conflict markers in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,7 @@ const nextConfig: NextConfig = {
   // Docker дүрсийг жижиг байлгах — docker-compose доторх app сервис үүнийг ашиглана.
   output: "standalone",
   serverActions: {
-<<<<<<< HEAD
     allowedOrigins: ["burtgel.dornogovi.gov.mn", "192.168.137.10:3007"],
-=======
-    allowedOrigins: ["burtgel.dornogovi.gov.mn"],
->>>>>>> 46a4aa2ba03e345ecf4761c2a47813b56ceac224
   },
 };
 
